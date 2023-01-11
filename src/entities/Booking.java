3fb830ced718ddf3541465818b2368fcb3206b89
2dam <2dam@TartangaLH.eus>
2023-01-11 09:55:10 +0100

    changes to Booking bean

--- src/entities/Booking.java
+++ src/entities/Booking.java
@@ -4,40 +4,16 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-@Entity
-@Table(name="booking",schema="storio")
-//@XmlRootElement
-@XmlRootElement
 public class Booking implements Serializable {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    //@ManyToOne
-    //@JoinColumn(name = "id")
     //private Client user;
-    @ManyToMany(mappedBy = "bookings")
     private List<Pack> packs;
-    @Temporal(javax.persistence.TemporalType.DATE)
     private Date startDate;
-    @Temporal(javax.persistence.TemporalType.DATE)
     private Date endDate;
     private String description;
-    @Enumerated(EnumType.STRING)
     private BookingState state;
     
     
