9316a357ebd72d45146c2260cd94bd148881fa4b
2dam <2dam@TartangaLH.eus>
2023-01-18 08:15:49 +0100

    added the windows and the restful for booking

---
+++ src/services/BookingClient.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+/**
+ * Jersey REST client generated for REST resource:BookingFacadeREST
+ * [entities.booking]<br>
+ * USAGE:
+ * <pre>
+ *        BookingClient client = new BookingClient();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author 2dam
+ */
+public class BookingClient {
+    
+}
