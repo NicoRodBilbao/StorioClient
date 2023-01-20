55c8aaae4d16697051eb58bac99342ff1031e061
2dam <2dam@TartangaLH.eus>
2023-01-20 11:44:24 +0100

    deleted implementations package

--- src/factories/BookingFactory.java
+++ src/factories/BookingFactory.java
@@ -5,10 +5,22 @@
  */
 package factories;
 
+import implementations.BookingImplementation;
+import interfaces.Bookingable;
+
 /**
  *
  * @author Markel Fernandez
  */
 public class BookingFactory {
+    private static Bookingable booking = new BookingImplementation();
     
+    /**
+     * This method creates an interface instanced as a Data Access Object.
+     *
+     * @return interface Bookingable as BookingImplementation
+     */
+    public static Bookingable getAccessBooking() {
+        return booking;
+    }
 }
