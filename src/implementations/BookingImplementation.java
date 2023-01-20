6e677fa1ba77c875fc4cedea5560edc2921e1187
2dam <2dam@TartangaLH.eus>
2023-01-20 10:05:15 +0100

    added implementations folder

--- src/implementations/BookingImplementation.java
+++ src/implementations/BookingImplementation.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package interfaces;
+package implementations;
 
 /**
  *
  * @author 2dam
  */
-public interface BookingInterface {
+public class BookingImplementation {
     
 }
--- src/interfaces/Bookingable.java
+++ src/interfaces/Bookingable.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package factories;
+package interfaces;
 
 /**
  *
  * @author Markel Fernandez
  */
-public class BookingFactory {
+public interface Bookingable {
     
 }
