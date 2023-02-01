487bbd567815f9771e1e56b6480e4d1916e2b3a4
2dam <2dam@TartangaLH.eus>
2023-02-01 14:23:50 +0100

    help window done

---
+++ src/windowController/HelpController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package windowController;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ *
+ * @author 2dam
+ */
+public class HelpController {
+    /**
+     * The control that shows the help page.
+     */
+    @FXML
+    private WebView webView;
+    /**
+     * Initializes and show the help window.
+     * @param root The FXML document hierarchy root. 
+     */
+    public void initAndShowStage(Parent root) {
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.setTitle("Ayuda para la Gestion de Usuarios");
+        stage.setResizable(false);
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+        stage.setOnShowing(this::handleWindowShowing);
+        stage.show();
+    }
+    /**
+     * Initializes window state. It implements behavior for WINDOW_SHOWING type 
+     * event.
+     * @param event  The window event 
+     */
+    private void handleWindowShowing(WindowEvent event){
+        WebEngine webEngine = webView.getEngine();
+        //Load help page.
+        webEngine.load(getClass()
+                .getResource("windowController/HelpBooking.html").toExternalForm());
+    }
+}
